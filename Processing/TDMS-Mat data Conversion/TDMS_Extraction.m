da7628b51f5b65d6fde8067ec24cf1bfa44eefac
thobbs13 <taylorhobbs13@gmail.com>
2025-03-21 13:15:01 -0400

    fix tdms extraction -CMG

--- Processing/TDMS-Mat data Conversion/TDMS_Extraction.m
+++ Processing/TDMS-Mat data Conversion/TDMS_Extraction.m
@@ -4,11 +4,11 @@ addpath(genpath('P:\users\tgh28\ChartWithCharles'))
 addpath 'C:\git\climber\src\Common\Matlab'
 addpath(genpath('C:\git\rtma'))
 
-tld = 'P:\data_raw\human\crs_array\CRS02b\OpenLoopStim';
+%tld = 'P:\data_raw\human\crs_array\CRS02b\OpenLoopStim';
+tld = 'P:\data_raw\human\crs_array\CRS02b\MPL_Experiments';
 output_folder = 'blah';
 
 flist = dir(tld); flist = flist(3:end);
-successful_extraction = false(size(flist));
 for f = 1:length(flist)
     fflist = dir(fullfile(tld, flist(f).name));
     % Check if raw files exist
@@ -16,22 +16,10 @@ for f = 1:length(flist)
         continue
     end
     % Convert TDMS -> Mat
-    try
+    disp(fullfile(tld, flist(f).name))
+    try        
         TDMSpostprocessing(fullfile(tld, flist(f).name), fullfile(tld, flist(f).name));
-    
-        % Combine .mat files and move to output folder
-        fflist = dir(fullfile(tld, flist(f).name));
-        idx = startsWith({fflist.name}, 'DAQ') & endsWith({fflist.name}, '.mat');
-        fflist = fflist(idx);
-        temp = struct();
-        for i = 1:length(fflist)
-            load(fullfile(fflist(i).folder, fflist(i).name))
-            %%% Start working from here
-            % Check if all amps are 10 or 20
-            % Add to the structure of the same format
-            break
-        end
-        successful_extraction(f) = true;
+        disp('success')
     catch
         disp('boo')
     end
