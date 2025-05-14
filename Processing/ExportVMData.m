8caf017a0c15cf3cd4be3ceb2d0cc2517446e41d
thobbs13 <taylorhobbs13@gmail.com>
2025-05-14 16:13:57 -0400

    Update ExportVMData.m
    Co-Authored-By: Charles M. Greenspon <24966610+CMGreenspon@users.noreply.github.com>

--- Processing/ExportVMData.m
+++ Processing/ExportVMData.m
@@ -1,21 +1,22 @@
 addpath(genpath(fullfile(getenv('STIMULATION_MODULES'), 'legacy\VoltageMonitor\utilities')))
 %%% Export Voltage Monitor Data
-tld = "P:\users\tgh28\Experiments\Longitudinal_ICMS\vm_data";
+tld = "P:\users\tgh28\Experiments\Longitudinal_ICMS\archive\vm_data";
 
 % List of sessions/collections
-vm_collection_list = dir(fullfile(tld));
+vm_collection_list = dir(fullfile(tld, '*.json'));
 for collection = 1:size(vm_collection_list,1)
     if contains(vm_collection_list(collection).name, 'session') == 0
         continue
     end
-    fname = sprintf('%s.mat', vm_collection_list(collection).name);
+    [~,b,c] = fileparts(vm_collection_list(collection).name);
+    fname = sprintf('%s.mat', b);
     
-    if exist(fullfile(tld, fname), 'file') == 2 || exist(fullfile(tld, vm_collection_list(collection).name), 'file') == 2
+    if exist(fullfile(tld, fname), 'file') == 2% || exist(fullfile(tld, vm_collection_list(collection).name), 'file') == 2
         continue
     end
 
     % Load the JSON
-    fprintf('Loading %s\n', fname)
+    fprintf('Loading %s\n', vm_collection_list(collection).name)
     try
         VMData = VoltageMonitor_JSON2MAT(fullfile(vm_collection_list(collection).folder,...
                                               vm_collection_list(collection).name));
