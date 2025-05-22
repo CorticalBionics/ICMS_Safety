b42f1f664ecb18ce6c4c7ab197002b7bf607880a
CeciVerbaarschot <ceciverbaarschot@gmail.com>
2025-05-22 11:32:28 -0500

    Update Figure1-and-SupplementaryFigure3_percept_quality.m
    Removed raw data processing and changed subject names to match the paper (P2 rather than CRS02b)

--- Analysis/Figure1-and-SupplementaryFigure3_percept_quality.m
+++ Analysis/Figure1-and-SupplementaryFigure3_percept_quality.m
@@ -2,11 +2,7 @@
 clear all;
 close all;
 
-data_folder = '/Users/verbaar/Documents/UPgh_Pittsburgh_Rubicon/surveys/raw_data/'; % path to the data
-subject = 'CRS02b'; % can be: CRS02b, CRS07, CRS08, BCI02, or BCI03
-
-% read in survey quality data
-[resp_data,response_header,s_date] = read_in_qualiy_data_final(data_folder,subject);
+data_folder = 'processed_data/'; % path to the data, e.g. P2_quality.mat
 
 % analyze quality data
 % Figure 1F and Supplementary Figure 3b-e
@@ -15,6 +11,7 @@ threshold = 30; % electrodes that evoke a sensation less than 30% of the time ar
 
 % permute quality over time
 % Supplementary Figure 3A
+subject = 'P2'; % can be 'P2', 'P3', 'P4', 'C1','C2'
 [mean_corr, std_corr, p] = permute_quality_over_time(data_folder,subject,freq_sensation);
 
 
