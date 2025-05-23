df2536ada945e708d5ec6b14c010fe554cd8e96e
CeciVerbaarschot <ceciverbaarschot@gmail.com>
2025-05-23 10:29:34 -0500

    Update quality_long_term.m
    Added the stacked bar plot to show quality stability across years

--- Analysis/quality_long_term.m
+++ Analysis/quality_long_term.m
@@ -100,6 +100,137 @@ xlim([0.5 2.5]);
 ylabel('pain intensity')
 set(gca,'FontSize',15);
 
+% get frequency of reported quality per year of implantation
+dates = P2_flat_dates;
+scores = P2_flat_scores;
+clear freq1 pain_intensity1
+[first,second,third,fourth,fifth,sixth,seventh,eight,ninth,last] = sort_dates(datetime(dates)); % get 4 equally spaced dates
+[freq1(1,:,:), pain_intensity1(1,:)] = get_freq_qualities(scores(datetime(dates)<=first,:),freq_sensation); % get frequency of quality across this time range
+[freq1(2,:,:), pain_intensity1(2,:)] = get_freq_qualities(scores(datetime(dates)>first & datetime(dates)<=second,:),freq_sensation); % get frequency of quality across this time range
+[freq1(3,:,:), pain_intensity1(3,:)] = get_freq_qualities(scores(datetime(dates)>second & datetime(dates)<=third,:),freq_sensation); % get frequency of quality across this time range
+[freq1(4,:,:), pain_intensity1(4,:)] = get_freq_qualities(scores(datetime(dates)>third & datetime(dates)<=fourth,:),freq_sensation); % get frequency of quality across this time range
+[freq1(5,:,:), pain_intensity1(5,:)] = get_freq_qualities(scores(datetime(dates)>fourth & datetime(dates)<=fifth,:),freq_sensation); % get frequency of quality across this time range
+[freq1(6,:,:), pain_intensity1(6,:)] = get_freq_qualities(scores(datetime(dates)>fifth & datetime(dates)<=sixth,:),freq_sensation); % get frequency of quality across this time range
+[freq1(7,:,:), pain_intensity1(7,:)] = get_freq_qualities(scores(datetime(dates)>sixth & datetime(dates)<=seventh,:),freq_sensation); % get frequency of quality across this time range
+[freq1(8,:,:), pain_intensity1(8,:)] = get_freq_qualities(scores(datetime(dates)>seventh & datetime(dates)<=eight,:),freq_sensation); % get frequency of quality across this time range
+[freq1(9,:,:), pain_intensity1(9,:)] = get_freq_qualities(scores(datetime(dates)>eight & datetime(dates)<=ninth,:),freq_sensation); % get frequency of quality across this time range
+[freq1(10,:,:), pain_intensity1(10,:)] = get_freq_qualities(scores(datetime(dates)>ninth,:),freq_sensation); % get frequency of quality across this time range
+
+dates = P3_flat_dates;
+scores = P3_flat_scores;
+clear freq2 pain_intensity2
+[first,second,third,fourth,fifth,sixth,seventh,eight,ninth,last] = sort_dates(datetime(dates)); % get 4 equally spaced dates
+[freq2(1,:,:), pain_intensity2(1,:)] = get_freq_qualities(scores(datetime(dates)<=first,:),freq_sensation); % get frequency of quality across this time range
+[freq2(2,:,:), pain_intensity2(2,:)] = get_freq_qualities(scores(datetime(dates)>first & datetime(dates)<=second,:),freq_sensation); % get frequency of quality across this time range
+[freq2(3,:,:), pain_intensity2(3,:)] = get_freq_qualities(scores(datetime(dates)>second & datetime(dates)<=third,:),freq_sensation); % get frequency of quality across this time range
+[freq2(4,:,:), pain_intensity2(4,:)] = get_freq_qualities(scores(datetime(dates)>third & datetime(dates)<=fourth,:),freq_sensation); % get frequency of quality across this time range
+[freq2(5,:,:), pain_intensity2(4,:)] = get_freq_qualities(scores(datetime(dates)>fourth,:),freq_sensation); % get frequency of quality across this time range
+
+dates = P4_flat_dates;
+scores = P4_flat_scores;
+clear freq3 pain_intensity3
+[first,second,third,fourth,fifth,sixth,seventh,eight,ninth,last] = sort_dates(datetime(dates)); % get 4 equally spaced dates
+[freq3(1,:,:), pain_intensity3(1,:)] = get_freq_qualities(scores(datetime(dates)<=first,:),freq_sensation); % get frequency of quality across this time range
+[freq3(2,:,:), pain_intensity3(2,:)] = get_freq_qualities(scores(datetime(dates)>first,:),freq_sensation); % get frequency of quality across this time range
+
+dates = C1_flat_dates;
+scores = C1_flat_scores;
+clear freq4 pain_intensity4
+[first,second,third,fourth,fifth,sixth,seventh,eight,ninth,last] = sort_dates(datetime(dates)); % get 4 equally spaced dates
+[freq4(1,:,:), pain_intensity4(1,:)] = get_freq_qualities(scores(datetime(dates)<=first,:),freq_sensation); % get frequency of quality across this time range
+[freq4(2,:,:), pain_intensity4(2,:)] = get_freq_qualities(scores(datetime(dates)>first & datetime(dates)<=second,:),freq_sensation); % get frequency of quality across this time range
+[freq4(3,:,:), pain_intensity4(3,:)] = get_freq_qualities(scores(datetime(dates)>second & datetime(dates)<=third,:),freq_sensation); % get frequency of quality across this time range
+[freq4(4,:,:), pain_intensity4(4,:)] = get_freq_qualities(scores(datetime(dates)>third & datetime(dates)<=fourth,:),freq_sensation); % get frequency of quality across this time range
+[freq4(5,:,:), pain_intensity4(5,:)] = get_freq_qualities(scores(datetime(dates)>fourth,:),freq_sensation); % get frequency of quality across this time range
+
+dates = C2_flat_dates;
+scores = C2_flat_scores;
+clear freq5 pain_intensity5
+[first,second,third,fourth,fifth,sixth,seventh,eight,ninth,last] = sort_dates(datetime(dates)); % get 4 equally spaced dates
+[freq5(1,:,:), pain_intensity5(1,:)] = get_freq_qualities(scores(datetime(dates)<=first,:),freq_sensation); % get frequency of quality across this time range
+[freq5(2,:,:), pain_intensity5(2,:)] = get_freq_qualities(scores(datetime(dates)>first,:),freq_sensation); % get frequency of quality across this time range
+
+% bar plot of quality over time per participant
+qidx = [2,3,4,5,6,7,8,11,12,14]; % relevant qualities (skipping over sparkle, warm, cold, and flutter)
+data1 = [sum(squeeze(freq1(1,:,qidx))>threshold); ...
+        sum(squeeze(freq1(2,:,qidx))>threshold); ...
+        sum(squeeze(freq1(3,:,qidx))>threshold); ...
+        sum(squeeze(freq1(4,:,qidx))>threshold); ...
+        sum(squeeze(freq1(5,:,qidx))>threshold); ...
+        sum(squeeze(freq1(6,:,qidx))>threshold); ...
+        sum(squeeze(freq1(7,:,qidx))>threshold); ...
+        sum(squeeze(freq1(8,:,qidx))>threshold); ...
+        sum(squeeze(freq1(9,:,qidx))>threshold); ...
+        sum(squeeze(freq1(10,:,qidx))>threshold)];
+data1 = (data1./sum(data1,2))*100;
+order = [10 9 8 7 6 5 4 3 1 2];
+data1 = data1(:,order);
+idx = find(sum(data1)>0);
+data1 = data1(:,idx);
+labels = {'touch','pressure','tapping','poke','sharp','vibration','buzzing','electrical','tickle','tingle'};
+labels = labels(order);
+labels = labels(idx);
+figure;  bar(data1,'stacked'); 
+legend(labels);
+ylim([0 100])
+
+data2 = [sum(squeeze(freq2(1,:,qidx))>threshold); ...
+        sum(squeeze(freq2(2,:,qidx))>threshold); ...
+        sum(squeeze(freq2(3,:,qidx))>threshold); ...
+        sum(squeeze(freq2(4,:,qidx))>threshold);];
+data2 = (data2./sum(data2,2))*100;
+data2 = data2(:,order);
+idx = find(sum(data2)>0);
+data2 = data2(:,idx);
+labels = {'touch','pressure','tapping','poke','sharp','vibration','buzzing','electrical','tickle','tingle'};
+labels = labels(order);
+labels = labels(idx);
+figure; bar(data2,'stacked');
+legend(labels);
+ylim([0 100])
+
+data3 = [sum(squeeze(freq3(1,:,qidx))>threshold); ...
+        sum(squeeze(freq3(2,:,qidx))>threshold)];
+data3 = (data3./sum(data3,2))*100;
+data3 = data3(:,order);
+idx = find(sum(data3)>0);
+data3 = data3(:,idx);
+labels = {'touch','pressure','tapping','poke','sharp','vibration','buzzing','electrical','tickle','tingle'};
+labels = labels(order);
+labels = labels(idx);
+figure; bar(data3,'stacked'); 
+legend(labels);
+ylim([0 100])
+
+data4 = [sum(squeeze(freq4(1,:,qidx))>threshold); ...
+        sum(squeeze(freq4(2,:,qidx))>threshold); ...
+        sum(squeeze(freq4(3,:,qidx))>threshold); ...
+        sum(squeeze(freq4(4,:,qidx))>threshold); ...
+        sum(squeeze(freq4(5,:,qidx))>threshold)];
+data4 = (data4./sum(data4,2))*100;
+data4 = data4(:,order);
+idx = find(sum(data4)>0);
+data4 = data4(:,idx);
+labels = {'touch','pressure','tapping','poke','sharp','vibration','buzzing','electrical','tickle','tingle'};
+labels = labels(order);
+labels = labels(idx);
+figure; bar(data4,'stacked'); 
+legend(labels);
+ylim([0 100])
+
+data5 = [sum(squeeze(freq5(1,:,qidx))>threshold); ...
+        sum(squeeze(freq5(2,:,qidx))>threshold)];
+data5 = (data5./sum(data5,2))*100;
+data5 = data5(:,order);
+idx = find(sum(data5)>0);
+data5 = data5(:,idx);
+labels = {'touch','pressure','tapping','poke','sharp','vibration','buzzing','electrical','tickle','tingle'};
+labels = labels(order);
+labels = labels(idx);
+figure; bar(data5,'stacked'); 
+legend(labels);
+ylim([0 100])
+
 % calc naturalness per year
 dates = P2_flat_dates;
 scores = P2_flat_scores;
