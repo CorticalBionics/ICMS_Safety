acc875d5666f80d47dcd600117f7e2723db43e87
CMGreenspon <charles.greenspon@gmail.com>
2025-05-28 13:27:20 -0500

    split processing and analysis

---
+++ Analysis/SignalQuality_Analysis.m
@@ -0,0 +1,142 @@
+%%% Signal quality analysis
+subjects = {'BCI02', 'BCI03', 'CRS02', 'CRS07', 'CRS08'};
+num_participants = length(subjects);
+% Load SQ, VM, and Cleaning Data
+% Get list of SQ data
+flist = dir(fullfile(DataPath, 'SignalQuality', '*.mat'));
+SQData = cell(num_participants, 1);
+
+% Load voltage data
+load(fullfile(DataPath, "VMData_All.mat"));
+VMData = data;
+conversion_factor = 1e6;
+total_charge = zeros(length(subjects), 64);
+
+% Load cleaning data
+load(fullfile(DataPath, 'CleaningData'));
+% Remove data from before 14-Aug-2017 (different monitoring system)
+min_date = datetime(736920, 'ConvertFrom', 'datenum');
+for p = 1:num_participants
+    idx = [cleaning_data{p}.date] > min_date;
+    cleaning_data{p} = cleaning_data{p}(idx); %#ok<SAGROW>
+end
+
+% Load SQData and process VM data
+implant_dates = NaT(num_participants, 1);
+for pi = 1:num_participants
+    % Load SQ Data
+    SQData{pi} = load(fullfile(DataPath, 'SignalQuality', flist(pi).name));
+    SQData{pi}.participant = flist(pi).name(1:5);
+    implant_dates(pi) = datetime(SQData{pi}.implant_metadata.implant_date, 'Format', 'dd-MMM-uuuu');
+
+    % Filter VM data by participant
+    s_idx = strcmp(data.Subject, subjects(pi));
+    total_current = cat(2, data.CurrentCount{s_idx});
+    all_charge = total_current .*  0.2 ./ conversion_factor; % Convert to charge in mC
+    total_charge(pi, :) = sum(all_charge, 2, 'omitnan');
+end
+SQData = cat(1, SQData{:});
+
+clearvars data all_charge total_current s_idx pi
+
+%% Analyze SNR/VPP/Cleaning
+[SNR_r, SNR_rp, SNR_slope, Vpp_r, Vpp_rp, Vpp_slope] = deal(NaN(256, num_participants));
+[Cln_r, Cln_rp, Cln_slope] = deal(NaN(64, num_participants));
+[median_SNR, median_Vpp, median_vinter] = deal(cell(num_participants, 1));
+[med_SNR_r, med_SNR_p, med_Vpp_r, med_Vpp_p, med_Cln_r, med_Cln_p] = deal(NaN(num_participants, 1));
+
+correlation_type = 'spearman';
+for pi = 1:num_participants
+    % Get sensory and motor masks
+    sens_idx = contains(SQData(pi).implant_metadata.array_names, 'sensory', 'IgnoreCase', true);
+    sensory_mask = SQData(pi).implant_metadata.chan_indices(sens_idx);
+    sensory_mask = cat(2, sensory_mask{:});
+    motor_idx = contains(SQData(pi).implant_metadata.array_names, 'motor', 'IgnoreCase', true);
+    motor_mask = SQData(pi).implant_metadata.chan_indices(motor_idx);
+    motor_mask = cat(2, motor_mask{:});
+
+    %%% SNR
+    x = datetime(num2str(SQData(pi).session_dates'), 'Format', 'yyyyMMdd');
+    x = years(x - x(1));
+    y = cat(1, SQData(pi).signal_quality_analysis.ch_snr);
+    y_snr = 10.^(y./20); % Undo log scaling
+    %%% Vpp
+    y_vpp = cat(1, SQData(pi).signal_quality_analysis.ch_vpp); % Same x as SNR
+    
+    % Correlations and slopes
+    [SNR_r(:,pi), SNR_rp(:,pi)] = corr(x, y_snr, 'Rows', 'pairwise', 'type', correlation_type);
+    [Vpp_r(:,pi), Vpp_rp(:,pi)] = corr(x, y_vpp, 'Rows', 'pairwise', 'type', correlation_type);
+
+    for c = 1:256
+        SNR_slope(c, pi) = nan_regression(x, y_snr(:,c));
+        Vpp_slope(c, pi) = nan_regression(x, y_vpp(:,c));
+    end   
+
+    % Smooth & subsample SNR
+    y_snr = movmedian(y_snr, 3, 1, 'omitnan');
+    median_SNR{pi}.sensory = median(y_snr(:, sensory_mask), 2, 'omitnan');
+    median_SNR{pi}.motor = median(y_snr(:, motor_mask), 2, 'omitnan');
+    % Vpp
+    y_vpp = movmedian(y_vpp, 3, 1, 'omitnan');
+    median_Vpp{pi}.sensory = median(y_vpp(:, sensory_mask), 2, 'omitnan');
+    median_Vpp{pi}.motor = median(y_vpp(:, motor_mask), 2, 'omitnan');
+    
+
+    %%% Cleaning
+    idx = cellfun(@(c) ~isempty(c), {cleaning_data{pi}.vmin});
+    x = [cleaning_data{pi}(idx).date];
+    x = years(x - x(1));
+    y = cat(2, [cleaning_data{pi}(idx).vinter]);
+    y(y < -1.5) = NaN; % Disconnected channels
+    for c = 1:64
+        Cln_slope(c, pi) = nan_regression(x, y(c,:));
+    end
+    [Cln_r(:,pi), Cln_rp(:,pi)] = corr(x', y', 'Rows', 'pairwise', 'type', correlation_type);
+    median_vinter{pi} = median(y, 1, 'omitmissing');
+    [med_Cln_r(pi), med_Cln_p(pi)] = corr(x', median_vinter{pi}', 'Rows', 'complete');
+    
+end
+
+% Holm Bonferroni correction
+SNR_rp = HolmBonferroni(SNR_rp);
+Vpp_rp = HolmBonferroni(Vpp_rp);
+Cln_rp = HolmBonferroni(Cln_rp);
+med_Cln_p = med_Cln_p * num_participants;
+
+% Store in structure and save
+SQAnalysis = struct();
+SQAnalysis.SNR_r = SNR_r;
+SQAnalysis.SNR_rp = SNR_rp;
+SQAnalysis.SNR_slope = SNR_slope;
+SQAnalysis.Vpp_r = Vpp_r;
+SQAnalysis.Vpp_rp = Vpp_rp;
+SQAnalysis.Vpp_slope = Vpp_slope;
+SQAnalysis.Cln_r = Cln_r;
+SQAnalysis.Cln_rp = Cln_rp;
+SQAnalysis.Cln_slope = Cln_slope;
+SQAnalysis.median_SNR = median_SNR;
+SQAnalysis.median_Vpp = median_Vpp;
+SQAnalysis.median_vinter = median_vinter;
+SQAnalysis.med_SNR_r = med_SNR_r;
+SQAnalysis.med_SNR_p = med_SNR_p;
+SQAnalysis.med_Vpp_r = med_Vpp_r;
+SQAnalysis.med_Vpp_p = med_Vpp_p;
+SQAnalysis.med_Cln_r = med_Cln_r;
+SQAnalysis.med_Cln_p = med_Cln_p;
+
+save(fullfile(DataPath, 'SQ_Analysis'), "SQAnalysis")
+
+
+%% Helper functions
+function slope = nan_regression(x, y)
+    slope = NaN;
+    nan_idx = isnan(y);
+    if sum(~nan_idx) < 10
+        return
+    end
+    if ~all(size(y) == size(x))
+        y = y';
+    end
+    pf = polyfit(x(~nan_idx)', y(~nan_idx)', 1);
+    slope = pf(1);
+end
--- Figures/SupplementaryFigure4and5_SignalQuality.m
+++ Figures/SupplementaryFigure4and5_SignalQuality.m
@@ -2,110 +2,6 @@
 subjects = {'BCI02', 'BCI03', 'CRS02', 'CRS07', 'CRS08'};
 subject_alt = {'C1', 'C2', 'P2' 'P3', 'P4'};
 num_participants = length(subjects);
-num_arrays = 4;
-
-%% Load SQ, VM, and Cleaning Data
-% Get list of SQ data
-flist = dir(fullfile(DataPath, 'SignalQuality', '*.mat'));
-SQData = cell(num_participants, 1);
-
-% Load voltage data
-load(fullfile(DataPath, "VMData_All.mat"));
-VMData = data;
-conversion_factor = 1e6;
-total_charge = zeros(length(subjects), 64);
-
-% Load cleaning data
-load(fullfile(DataPath, 'CleaningData'));
-% Remove data from before 14-Aug-2017 (different monitoring system)
-min_date = datetime(736920, 'ConvertFrom', 'datenum');
-for p = 1:num_participants
-    idx = [cleaning_data{p}.date] > min_date;
-    cleaning_data{p} = cleaning_data{p}(idx); %#ok<SAGROW>
-end
-
-% Load SQData and process VM data
-implant_dates = NaT(num_participants, 1);
-for pi = 1:num_participants
-    % Load SQ Data
-    SQData{pi} = load(fullfile(DataPath, 'SignalQuality', flist(pi).name));
-    SQData{pi}.participant = flist(pi).name(1:5);
-    implant_dates(pi) = datetime(SQData{pi}.implant_metadata.implant_date, 'Format', 'dd-MMM-uuuu');
-
-    % Filter VM data by participant
-    s_idx = strcmp(data.Subject, subjects(pi));
-    total_current = cat(2, data.CurrentCount{s_idx});
-    all_charge = total_current .*  0.2 ./ conversion_factor; % Convert to charge in mC
-    total_charge(pi, :) = sum(all_charge, 2, 'omitnan');
-end
-SQData = cat(1, SQData{:});
-
-clearvars data all_charge total_current s_idx pi
-
-%% Analyze SNR/VPP/Cleaning
-[SNR_r, SNR_rp, SNR_slope, Vpp_r, Vpp_rp, Vpp_slope] = deal(NaN(256, num_participants));
-[Cln_r, Cln_rp, Cln_slope] = deal(NaN(64, num_participants));
-[median_SNR, median_Vpp, median_vinter] = deal(cell(num_participants, 1));
-[med_SNR_r, med_SNR_p, med_Vpp_r, med_Vpp_p, med_Cln_r, med_Cln_p] = deal(NaN(num_participants, 1));
-
-correlation_type = 'spearman';
-for pi = 1:num_participants
-    % Get sensory and motor masks
-    sens_idx = contains(SQData(pi).implant_metadata.array_names, 'sensory', 'IgnoreCase', true);
-    sensory_mask = SQData(pi).implant_metadata.chan_indices(sens_idx);
-    sensory_mask = cat(2, sensory_mask{:});
-    motor_idx = contains(SQData(pi).implant_metadata.array_names, 'motor', 'IgnoreCase', true);
-    motor_mask = SQData(pi).implant_metadata.chan_indices(motor_idx);
-    motor_mask = cat(2, motor_mask{:});
-
-    %%% SNR
-    x = datetime(num2str(SQData(pi).session_dates'), 'Format', 'yyyyMMdd');
-    x = years(x - x(1));
-    y = cat(1, SQData(pi).signal_quality_analysis.ch_snr);
-    y_snr = 10.^(y./20); % Undo log scaling
-    %%% Vpp
-    y_vpp = cat(1, SQData(pi).signal_quality_analysis.ch_vpp); % Same x as SNR
-    
-    % Correlations and slopes
-    [SNR_r(:,pi), SNR_rp(:,pi)] = corr(x, y_snr, 'Rows', 'pairwise', 'type', correlation_type);
-    [Vpp_r(:,pi), Vpp_rp(:,pi)] = corr(x, y_vpp, 'Rows', 'pairwise', 'type', correlation_type);
-
-    for c = 1:256
-        SNR_slope(c, pi) = nan_regression(x, y_snr(:,c));
-        Vpp_slope(c, pi) = nan_regression(x, y_vpp(:,c));
-    end   
-
-    % Smooth & subsample SNR
-    y_snr = movmedian(y_snr, 3, 1, 'omitnan');
-    median_SNR{pi}.sensory = median(y_snr(:, sensory_mask), 2, 'omitnan');
-    median_SNR{pi}.motor = median(y_snr(:, motor_mask), 2, 'omitnan');
-    % Vpp
-    y_vpp = movmedian(y_vpp, 3, 1, 'omitnan');
-    median_Vpp{pi}.sensory = median(y_vpp(:, sensory_mask), 2, 'omitnan');
-    median_Vpp{pi}.motor = median(y_vpp(:, motor_mask), 2, 'omitnan');
-    
-
-    %%% Cleaning
-    idx = cellfun(@(c) ~isempty(c), {cleaning_data{pi}.vmin});
-    x = [cleaning_data{pi}(idx).date];
-    x = years(x - x(1));
-    y = cat(2, [cleaning_data{pi}(idx).vinter]);
-    y(y < -1.5) = NaN; % Disconnected channels
-    for c = 1:64
-        Cln_slope(c, pi) = nan_regression(x, y(c,:));
-    end
-    [Cln_r(:,pi), Cln_rp(:,pi)] = corr(x', y', 'Rows', 'pairwise', 'type', correlation_type);
-    median_vinter{pi} = median(y, 1, 'omitmissing');
-    [med_Cln_r(pi), med_Cln_p(pi)] = corr(x', median_vinter{pi}', 'Rows', 'complete');
-    
-end
-
-% Holm Bonferroni correction
-SNR_rp = HolmBonferroni(SNR_rp);
-Vpp_rp = HolmBonferroni(Vpp_rp);
-Cln_rp = HolmBonferroni(Cln_rp);
-med_Cln_p = med_Cln_p * num_participants;
-
 
 %% Supplementary Figure 4
 [ax_size_y, ax_y_val] = GetAxisCoords(num_participants, 0.04, 0.05);
@@ -128,16 +24,7 @@ for p = 1:num_participants
     xl = [0, ceil(x(end))];
     xt = [xl(1):xl(end)];
     xl = [xl(1) - range(xl) * 0.05, xl(end) + range(xl) * 0.05];
-    xtl = cell(size(xt));
-    for i = 1:length(xtl)
-        if i == 1
-            xtl{i} = '0';
-        elseif i == length(xtl)
-            xtl{i} = num2str(i-1);
-        else
-            xtl{i} = '';
-        end
-    end
+    xtl = sparse_xticklabels(xt);
 
     % SNR
     axes('Position', [ax_x_val(1), ax_y_val(p), ax_size_x, ax_size_y]); hold on
@@ -347,17 +234,3 @@ end
 
 AddFigureLabels(h.Children([3,2,1]), [.07, .0275])
 % export_figure3x(FigurePath, 'SuppFig5_ChargeQuality')
-
-%% Functions
-function slope = nan_regression(x, y)
-    slope = NaN;
-    nan_idx = isnan(y);
-    if sum(~nan_idx) < 10
-        return
-    end
-    if ~all(size(y) == size(x))
-        y = y';
-    end
-    pf = polyfit(x(~nan_idx)', y(~nan_idx)', 1);
-    slope = pf(1);
-end
