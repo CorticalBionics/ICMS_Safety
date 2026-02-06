161ba039d60bd3de0b93755a67d5b6f3aa188dbc
CMGreenspon <charles.greenspon@gmail.com>
2026-02-06 06:22:26 -0600

    handle new electrode numbering and start ES cable checks

--- Processing/PulseCountExtraction.m
+++ Processing/PulseCountExtraction.m
@@ -2,58 +2,43 @@
 num_electrodes = 64;
 data_path = cc.load_config.system('backup_path');
 output_path = fullfile(DataPath, "VM_Info");
-subject_ids = {'BCI02', 'BCI03', 'CRS02b', 'CRS07', 'CRS08'};
+subject_ids = {'BCI02', 'BCI03'};
 temp = table('Size', [1, 8], ...
     'VariableNames', {'Subject', 'Date', 'PulseCount', 'CurrentCount', 'Duration', 'NumSingleElec', 'NumMultiElec', 'Waveforms'}, ...
     'VariableTypes', ["string", "datetime", "cell", "cell", "double", "double", "double", "cell"]);
 override = false;
 
 for s = 1:length(subject_ids)
+    elec_map = cc.analysis.load_participant_electrode_map(subject_ids{s});
+
     % Get contents of voltage monitor directory
     vm_path = fullfile(data_path, subject_ids{s}, 'VoltageMonitor');
-    if startsWith(subject_ids{s}, 'BCI')
-        flist = dir(fullfile(vm_path, 'VM*'));
-    elseif startsWith(subject_ids{s}, 'CRS')
-        flist = dir(fullfile(vm_path, '*.mat'));
-    end
+    flist = dir(fullfile(vm_path, 'VM*'));
     
     msg = '';
     for f = 1:length(flist)
-        msg = InlineProgressBar('Loading %d/%d', [f,length(flist)], msg);        
-        if startsWith(subject_ids{s}, 'BCI')
-            % Skip anything that isn't a subfolder
-            if ~flist(f).isdir
-                continue
-            end
-    
-            % Try to find mat file and get date
-            expected_fname = sprintf('%s_%s.mat', subject_ids{s}, flist(f).name);
-            expected_ffname = fullfile(vm_path, flist(f).name, expected_fname);
-            if isfile(fullfile(output_path, expected_fname)) & ~override
-                continue
-            elseif isfile(expected_ffname)
-                fsplit = strsplit(flist(f).name, '_');
-                dn = datetime([str2double(fsplit{2}), str2double(fsplit{3}), str2double(fsplit{4})]);
-                load(expected_ffname);
-            else
-                warning('No .mat file found in %s\n', flist(f).name)
-                msg = '';
-                continue
-            end
-        elseif startsWith(subject_ids{s}, 'CRS')
-            expected_fname = flist(f).name;
-            if isfile(fullfile(output_path, expected_fname)) & ~override
-                continue
-            end
-            load(fullfile(flist(f).folder, flist(f).name));
-            % Process date
-            fsplit = strsplit(flist(f).name, '_');
-            if contains(fsplit{end}, 'motor')
-                dn = datetime([fsplit{5}, fsplit{3}, fsplit{4}], 'InputFormat', 'yyyyMMdd');
-            else
-                dn = datetime([fsplit{5}(1:end-4), fsplit{3}, fsplit{4}], 'InputFormat', 'yyyyMMdd');
-            end
+        msg = InlineProgressBar('Loading %d/%d', [f,length(flist)], msg);
+
+        % Skip anything that isn't a subfolder
+        if ~flist(f).isdir
+            continue
         end
+
+        % Try to find mat file and get date
+        expected_fname = sprintf('%s_%s.mat', subject_ids{s}, flist(f).name);
+        expected_ffname = fullfile(vm_path, flist(f).name, expected_fname);
+        if isfile(fullfile(output_path, expected_fname)) & ~override
+            continue
+        elseif isfile(expected_ffname)
+            fsplit = strsplit(flist(f).name, '_');
+            dn = datetime([str2double(fsplit{2}), str2double(fsplit{3}), str2double(fsplit{4})]);
+            load(expected_ffname);
+        else
+            warning('No .mat file found in %s\n', flist(f).name)
+            msg = '';
+            continue
+        end
+
 
         % create vector for number of pulses and total amplitude
         [num_pulses, total_current] = deal(zeros(num_electrodes, 1));
@@ -62,12 +47,22 @@ for s = 1:length(subject_ids)
         
         % Running count
         for i = 1:length(VMData)
+            % Check for ES cables
+            % temp = 0;
+
             for c = 1:length(VMData(i).Channels)
                 % Sometimes motor exec sends fake pulses
-                if VMData(i).Channels(c) > num_electrodes || VMData(i).Channels(c) == 0 
+                if (strcmpi(VMData(i).SessionType, 'MotorExperiments') && VMData(i).Channels(c) > num_electrodes) || ...
+                        VMData(i).Channels(c) == 0 
                     continue
                 end
+                
                 c_idx = VMData(i).Channels(c);
+                % Check channel counting
+                if c_idx > 64 % We changed from reporting cerestim output to absolute electrode number
+                    c_idx = br2vm(c_idx, elec_map);
+                end
+                
                 num_pulses(c_idx) = num_pulses(c_idx) + sum(VMData(i).Amplitudes{c} > 0);
                 total_current(c_idx) = total_current(c_idx) + sum(VMData(i).Amplitudes{c});
             end
@@ -146,3 +141,12 @@ for pi = 1:length(subject_ids)
 end
 
 save(fullfile(DataPath, 'VMData_All'), "VMData", "total_charge")
+
+%% Helper function to convert new channel ids to old
+function elec = br2vm(elec, elec_map)
+    if ismember(elec, elec_map.lateral_sensory.locations)
+        elec = elec_map.lateral_sensory.numbers(elec_map.lateral_sensory.locations == elec);
+    elseif ismember(elec, elec_map.medial_sensory.locations)
+        elec = elec_map.medial_sensory.numbers(elec_map.medial_sensory.locations == elec);
+    end
+end
